fc1ba228a8c9c3ad83b6fa9acf66af597af97344
davidsiguenza <dsiguenza@salesforce.com>
2025-08-26 19:30:05 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      // MUY IMPORTANTE: Cambia <TU-NOMBRE-DE-REPOSITORIO> por el nombre de tu repositorio en GitHub.
-      // Por ejemplo, si tu repositorio está en https://github.com/tu-usuario/mi-jardin,
-      // deberías poner: base: '/mi-jardin/',
       base: '/Jard-n-rellas/',
       resolve: {
         alias: {
